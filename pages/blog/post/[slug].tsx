9a1969d90a6dc3dcac76b30137e7db80463ceb67
Azad Kshitij <azadkshitij08302001@gmail.com>
2022-03-19 23:44:13 +0530

    Deployment_1

--- pages/blog/post/[slug].tsx
+++ pages/blog/post/[slug].tsx
@@ -25,8 +25,6 @@ function Slug({ post: post }: { post: postComplete }) {
             <PostDetails post={post} />
             <Author author={post.author} />
             {/* <AdjacentPosts slug={post.slug} createdAt={post.createdAt} /> */}
-            {/* <CommentsForm slug={post.slug} /> */}
-            {/* <Comments slug={post.slug} /> */}
           </div>
           <div className="col-span-1 lg:col-span-4">
             <div className="relative top-8 lg:sticky">
